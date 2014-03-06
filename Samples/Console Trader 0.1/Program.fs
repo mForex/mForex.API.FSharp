5400b3ea909a12c1e3d43eccf2c1629acfa496bc
Wojciech Stokowiec <wojciech.stokowiec@hotmail.com>
2014-03-06 12:29:49 +0100

    api connect mehtod has been parameterized

--- Samples/Console Trader 0.1/Program.fs
+++ Samples/Console Trader 0.1/Program.fs
@@ -4,16 +4,15 @@ open System.Threading
 open System
 
 type Agent<'a> = MailboxProcessor<'a>
+type Message = | Start of Credentials: int * string
 
-type Message =
-    | Start    
-
-type Api() =    
+type Api() =        
+    
     let client = APIClient(false)
     
-    let connect() = async {
+    let connect login password = async {
             do! client.Connect()
-            let! loggedIn = client.Login(77000005, "Haslo123")
+            let! loggedIn = client.Login(login, password)
             return loggedIn
         }
 
@@ -22,29 +21,33 @@ type Api() =
                 let! msg = a.Receive()
                 
                 match msg with
-                | Start -> let! r  = connect()
-                           printfn "Logged in: %b" r.LoggedIn
+                | Start (login,password)  -> 
+                    let! r  = (connect login password)
+                    printfn "Logged in: %b" r.LoggedIn
         })
 
     
     do agent.Error.Add(fun e -> printfn "Fatal error occured: %O" e)
 
-    member this.Connect() =
-        agent.Post(Start) 
+    member this.Connect(login, password) =
+        agent.Post(Start (login, password)) 
 
     member this.Stream symbol =
         client.Ticks.Add(fun p -> 
             p |> Array.toList 
               |> List.filter (fun x -> x.Symbol = symbol)
-              |> List.iter (fun x -> printfn "%A %f/%f %A" x.Symbol x.Bid x.Ask x.Time))
+              |> List.iter (fun x -> printfn "%A %.5f/%.5f %A" x.Symbol x.Bid x.Ask x.Time))
         
 
 [<EntryPoint>]
 let main argv = 
+    let login = 77000005
+    let password = "Haslo123"
+    
     let api = Api()
     
-    api.Connect()    
+    api.Connect(login, password)    
     api.Stream "EURUSD"
 
-    Console.ReadLine() |> ignore
+    Console.ReadKey() |> ignore
     0
